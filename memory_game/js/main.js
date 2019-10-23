21788762d9e4e7cbf9ed271f0880b5215e2ae15b
Jos Bogan <josbogan@Joss-MBP.home>
2019-10-23 12:41:39 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,21 @@
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
+var cards =["Queen", "Queen", "King", "King"];
 
-console.log("User Flipped " + cardOne);
+var cardsInPlay = [];
 
-console.log("User Flipped " + cardThree);
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a Match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+}
+
+console.log(cardsInPlay[0]);
+console.log(cardsInPlay[1]);
