d9a2209acee4d03328fce897df53adbbc87f1383
Jos Bogan <josbogan@Joss-MBP.home>
2019-10-25 13:49:10 +0100

    added more game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,20 +2,22 @@ var cards =["Queen", "Queen", "King", "King"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a Match!");
-	}
-	else {
-		alert("Sorry, try again.");
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a Match!");
+		}
+		else {
+			alert("Sorry, try again.");
+		}
 	}
 }
 
-console.log(cardsInPlay[0]);
-console.log(cardsInPlay[1]);
+function flipCard(cardId) {
+	console.log("You flipped " + cards[cardId] + ".");
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
