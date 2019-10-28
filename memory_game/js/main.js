7528adc206639f40a599ae4adc346bd943010b71
Jos Bogan <josbogan@Joss-MBP.home>
2019-10-28 22:29:45 +0000

    Added reset and Shuffle buttons/functionality

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,3 +1,5 @@
+// Array of all possible cards
+
 var cards = [
 {
 	rank: "queen",
@@ -21,12 +23,27 @@ var cards = [
 }
 ];
 
-var cardsInPlay = [];
+var cardsInPlay = []; //Array for cards in play
+
+var shuffleCards = []; //Array for shuffled cards
+
+var score = 0; // Score Variable
+
+// shuffle cards array and set shuffled array
+
+function shuffle() {
+	shuffleCards = cards.sort(function(a,b){return 0.5 - Math.random()});
+}
+
+// function that checks for a match and adds to score
 
 function checkForMatch() {
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a Match!");
+			score++;
+			document.getElementById('score').textContent = score;
+
 		}
 		else {
 			alert("Sorry, try again.");
@@ -34,15 +51,20 @@ function checkForMatch() {
 	}
 }
 
+//function that relates the cardId to an index in the shuffled array, sets the image and funs the match check
+
 function flipCard() {
 	var cardId = this.getAttribute('data-id');
-	console.log("You flipped " + cards[cardId].rank + ".");
-	console.log(cards[cardId].suit);
-	console.log(cards[cardId].cardImage);
-	cardsInPlay.push(cards[cardId].rank);
-	this.setAttribute('src', cards[cardId].cardImage);
+	/* Debug --------
+	console.log("You flipped " + shuffleCards[cardId].rank + ".");
+	console.log(shuffleCards[cardId].suit);
+	console.log(shuffleCards[cardId].cardImage); */
+	cardsInPlay.push(shuffleCards[cardId].rank); 
+	this.setAttribute('src', shuffleCards[cardId].cardImage);
 	checkForMatch();
 }
+
+// function that creates the gameboard elements referencing the length of the cards array, adds the id and click event listener
 
 function createBoard() {
 	for (i = 0; i < cards.length; i++) {
@@ -54,5 +76,35 @@ function createBoard() {
 	}
 }
 
+// reset function, changes img to backs, and wipes the cards in play
+
+function resetFunc() {
+	var cardArray = document.getElementsByTagName('img');
+	for (i = 0; i < cardArray.length; i++) {
+		cardArray[i].setAttribute('src', 'images/back.png');
+	}
+	cardsInPlay = [];
+}
+
+// Shuffle function does the same as reset with shuffle involved as well
+
+function shuffleFunc() {
+	resetFunc();
+	shuffle();
+}
+
+// reset button event
+
+document.getElementById('reset').addEventListener('click', resetFunc);
+
+document.getElementById('shuffle').addEventListener('click', shuffleFunc);
+
+// Set initial Score
+
+document.getElementById('score').textContent = score;
+
+// Load functions
+
 createBoard();
+shuffle();
 
